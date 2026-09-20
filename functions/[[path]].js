fdf3752ac61d75e87c44381fcd5558c182e231f5
eshjw2018-cpu <eshjw2018@gmail.com>
2026-09-20 16:19:39 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,8 @@
 import worker from '../src/index.js';
 
 export async function onRequest(context) {
-  return worker.fetch(context.request, context.env, context);
+  return worker.fetch(context.request, context.env, {
+    waitUntil: context.waitUntil.bind(context),
+    passThroughOnException: () => {},
+  });
 }
